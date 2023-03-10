273aae3a55e6d54b3a31b73073035f2b78f95f2d
Aniket <70195348+AniketKharpatil@users.noreply.github.com>
2023-03-10 11:34:24 +0530

    Update index.js

--- evdetail/src/index.js
+++ evdetail/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-// import Prov from './Prov'
+
 import reportWebVitals from './reportWebVitals';
-import EVCharge from './Front';
+// import EVCharge from './Front';
+import EV from "./EV"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <EVCharge/>
+    <EV/>
   </React.StrictMode>
 );
 
